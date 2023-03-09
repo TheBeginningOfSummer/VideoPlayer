f688e01c1de21b341053b55c06d6a28d1e8cd380
May <May@Summer.Afternoon>
2023-03-09 09:46:14 +0800

    添加项目文件。

---
+++ WindowsPlayer/PlayerForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsPlayer
+{
+    partial class PlayerForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PN_Display = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BTN_Open = new System.Windows.Forms.Button();
+            this.BTN_Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PN_Display
+            // 
+            this.PN_Display.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PN_Display.BackColor = System.Drawing.Color.Black;
+            this.PN_Display.Location = new System.Drawing.Point(0, 0);
+            this.PN_Display.Margin = new System.Windows.Forms.Padding(0);
+            this.PN_Display.Name = "PN_Display";
+            this.PN_Display.Size = new System.Drawing.Size(654, 326);
+            this.PN_Display.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BTN_Open
+            // 
+            this.BTN_Open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BTN_Open.Location = new System.Drawing.Point(9, 331);
+            this.BTN_Open.Margin = new System.Windows.Forms.Padding(0);
+            this.BTN_Open.Name = "BTN_Open";
+            this.BTN_Open.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Open.TabIndex = 2;
+            this.BTN_Open.Text = "打开";
+            this.BTN_Open.UseVisualStyleBackColor = true;
+            this.BTN_Open.Click += new System.EventHandler(this.BTN_Open_Click);
+            // 
+            // BTN_Stop
+            // 
+            this.BTN_Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BTN_Stop.Location = new System.Drawing.Point(87, 331);
+            this.BTN_Stop.Name = "BTN_Stop";
+            this.BTN_Stop.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Stop.TabIndex = 3;
+            this.BTN_Stop.Text = "停止";
+            this.BTN_Stop.UseVisualStyleBackColor = true;
+            this.BTN_Stop.Click += new System.EventHandler(this.BTN_Stop_Click);
+            // 
+            // PlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 363);
+            this.Controls.Add(this.BTN_Stop);
+            this.Controls.Add(this.BTN_Open);
+            this.Controls.Add(this.PN_Display);
+            this.Name = "PlayerForm";
+            this.Text = "WindowsPlayer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PN_Display;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BTN_Open;
+        private System.Windows.Forms.Button BTN_Stop;
+    }
+}
+
---
+++ WindowsPlayer/PlayerForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsPlayer
+{
+    public partial class PlayerForm : Form
+    {
+        private readonly VlcPlayer vlc_player_;
+        private bool is_playinig_;
+
+        public PlayerForm()
+        {
+            InitializeComponent();
+
+            string pluginPath = Environment.CurrentDirectory + "\\plugins\\";
+            vlc_player_ = new VlcPlayer(pluginPath);
+            IntPtr render_wnd = this.PN_Display.Handle;
+            vlc_player_.SetRenderWindow((int)render_wnd);
+
+            //tbVideoTime.Text = "00:00:00/00:00:00";
+
+            is_playinig_ = false;
+        }
+
+        private void BTN_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                vlc_player_.PlayFile(ofd.FileName);
+                //trackBar1.SetRange(0, (int)vlc_player_.Duration());
+                //trackBar1.Value = 0;
+                //timer1.Start();
+                is_playinig_ = true;
+            }
+        }
+
+        private void BTN_Stop_Click(object sender, EventArgs e)
+        {
+            if (is_playinig_)
+            {
+                vlc_player_.Stop();
+                //trackBar1.Value = 0;
+                //timer1.Stop();
+                is_playinig_ = false;
+            }
+        }
+    }
+}
